e67186f20cc972e7494336480ac9e076f04c4c8c
ozimos <kharozim06@gmail.com>
2022-04-16 00:52:05 +0700

    configure obfuscation

--- app/src/main/java/com/ozimos/made/home/HomeFragment.kt
+++ app/src/main/java/com/ozimos/made/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.ozimos.made.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -92,7 +91,7 @@ class HomeFragment : Fragment() {
                     setData(it.data)
                 }
                 is Resourse.Error -> {
-                    binding.root.let { it1 -> showSnackBar(it1, it.msg ?: "error") }
+                    showSnackBar(binding.root, it.msg ?: "error")
                 }
             }
         }
